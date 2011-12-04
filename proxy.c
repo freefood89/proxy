b3567da5f07c6f9c0978a11824f19ea6619266b7
Benjamin Shih <bshih1@ww109-c7.ww.andrew.cmu.edu>
2011-12-03 21:07:29 -0500

    mine

--- proxy.c
+++ proxy.c
@@ -22,7 +22,7 @@ void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, 
-		 char *shortmsg, char *longmsg);
+        char *shortmsg, char *longmsg);
 
 void genheader(char *host, char *header); 
 void genrequest(char *request, char *method, char *uri, char *version);
@@ -32,24 +32,24 @@ void parseURL(char* url, char* host, char* uri);
  */
 int main(int argc, char **argv) 
 {
-  int listenfd, connfd, port, clientlen;
-  struct sockaddr_in clientaddr;
+    int listenfd, connfd, port, clientlen;
+    struct sockaddr_in clientaddr;
 
-  /* Check command line args */
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s <port>\n", argv[0]);
-    exit(1);
-  }
-  port = atoi(argv[1]);
+    /* Check command line args */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+    port = atoi(argv[1]);
 
-  listenfd = Open_listenfd(port);
-  clientlen = sizeof(clientaddr);  
-  connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-  while (1) {
-    doit(connfd);
+    listenfd = Open_listenfd(port);
+    clientlen = sizeof(clientaddr);  
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    while (1) {
+        doit(connfd);
 
-    Close(connfd);
-  }
+        Close(connfd);
+    }
 }
 
 /*
@@ -57,69 +57,69 @@ int main(int argc, char **argv)
  */
 void doit(int fd) 
 {
-  //int is_static;
-  //struct stat sbuf;
-  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-  //char filename[MAXLINE], cgiargs[MAXLINE];
-  rio_t rio_c, rio_h;
+    //int is_static;
+    //struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    //char filename[MAXLINE], cgiargs[MAXLINE];
+    rio_t rio_c, rio_h;
 
-  /* Ren's Local Vars */
-  char host[MAXLINE], url[MAXLINE], request[MAXLINE], header[MAXLINE];
-  int hostfd;
-  /* Ren's Local Vars END */
+    /* Ren's Local Vars */
+    char host[MAXLINE], url[MAXLINE], request[MAXLINE], header[MAXLINE];
+    int hostfd;
+    /* Ren's Local Vars END */
 
 
-  /* Read request line and headers */
-  Rio_readinitb(&rio_c, fd);
+    /* Read request line and headers */
+    Rio_readinitb(&rio_c, fd);
 
-  /* loop while readline did not get EOF or error
-   * getting EOF means client closed connection */
-  
-  while(Rio_readlineb(&rio_c, buf, MAXLINE)!=0){
-    sscanf(buf, "%s %s %s", method, url, version);
-    /* if (strcasecmp(method, "GET")) { 
-      clienterror(fd, method, "501", "Not Implemented",   
-		  "Tiny does not implement this method");
-      return;
-      }*/
-    printf("STUFF FROM THE CLIENT:\n");
-    printf("%s\n",buf);
-    read_requesthdrs(&rio_c);
+    /* loop while readline did not get EOF or error
+     * getting EOF means client closed connection */
 
-    /* Ren's code */
-    parseURL(url, host, uri); /* parse url for hostname and uri */
-    hostfd = Open_clientfd(host, PORT); /* connect to host as client */
-    Rio_readinitb(&rio_h, hostfd); /* set up host file discriptor */
+    while(Rio_readlineb(&rio_c, buf, MAXLINE)!=0){
+        sscanf(buf, "%s %s %s", method, url, version);
+        /* if (strcasecmp(method, "GET")) { 
+           clienterror(fd, method, "501", "Not Implemented",   
+           "Tiny does not implement this method");
+           return;
+           }*/
+        printf("STUFF FROM THE CLIENT:\n");
+        printf("%s\n",buf);
+        read_requesthdrs(&rio_c);
 
-    /* generate and send request to host*/  
-    genrequest(request, method, uri, version);
-    genheader(host, header);
-    strcat(request, header);
-    printf("STUFF TO THE SERVER:\n%s",request); 
-    Rio_writen(hostfd, request, strlen(request));
+        /* Ren's code */
+        parseURL(url, host, uri); /* parse url for hostname and uri */
+        hostfd = Open_clientfd(host, PORT); /* connect to host as client */
+        Rio_readinitb(&rio_h, hostfd); /* set up host file discriptor */
 
-    /* stream information from server to client */
-    printf("STUFF FROM THE SERVER:\n");
+        /* generate and send request to host*/  
+        genrequest(request, method, uri, version);
+        genheader(host, header);
+        strcat(request, header);
+        printf("STUFF TO THE SERVER:\n%s",request); 
+        Rio_writen(hostfd, request, strlen(request));
 
-    /* go through server response+header */
-    do{
-      Rio_readlineb(&rio_h,buf,MAXLINE);
-      printf("%s",buf);
-      Rio_writen(fd,buf, MAXLINE);
-    }while(strcmp(buf,"\r\n"));
-    printf("end of server response header\n");
+        /* stream information from server to client */
+        printf("STUFF FROM THE SERVER:\n");
 
-    while(Rio_readlineb(&rio_h, buf, MAXLINE)){
-      printf("%s",buf);
-      Rio_writen(fd, buf, MAXLINE);
+        /* go through server response+header */
+        do{
+            Rio_readlineb(&rio_h,buf,MAXLINE);
+            printf("%s",buf);
+            Rio_writen(fd,buf, MAXLINE);
+        }while(strcmp(buf,"\r\n"));
+        printf("end of server response header\n");
+
+        while(Rio_readlineb(&rio_h, buf, MAXLINE)){
+            printf("%s",buf);
+            Rio_writen(fd, buf, MAXLINE);
+        }
+        printf("\nstream ended\n");
+        //    Rio_writen(fd,"\r\n",MAXLINE); /*signify end of transmission */
+        //printf("sent carriage return\n");
+        Close(hostfd); /* disconnect from host */
+        printf("closed connection with host\n");
     }
-    printf("\nstream ended\n");
-    //    Rio_writen(fd,"\r\n",MAXLINE); /*signify end of transmission */
-    //printf("sent carriage return\n");
-    Close(hostfd); /* disconnect from host */
-    printf("closed connection with host\n");
-  }
-  printf("%s\n",buf);
+    printf("%s\n",buf);
 }
 
 /*
@@ -127,15 +127,15 @@ void doit(int fd)
  */
 void read_requesthdrs(rio_t *rp) 
 {
-  char buf[MAXLINE];
+    char buf[MAXLINE];
 
-  Rio_readlineb(rp, buf, MAXLINE);
-  printf("%s", buf);
-  while(strcmp(buf, "\r\n")) {
     Rio_readlineb(rp, buf, MAXLINE);
     printf("%s", buf);
-  }
-  return;
+    while(strcmp(buf, "\r\n")) {
+        Rio_readlineb(rp, buf, MAXLINE);
+        printf("%s", buf);
+    }
+    return;
 }
 
 
@@ -145,28 +145,28 @@ void read_requesthdrs(rio_t *rp)
  */
 int parse_uri(char *uri, char *filename, char *cgiargs) 
 {
-  char *ptr;
+    char *ptr;
 
-  if (!strstr(uri, "cgi-bin")) {  /* Static content */
-    strcpy(cgiargs, "");
-    strcpy(filename, ".");
-    strcat(filename, uri);
-    if (uri[strlen(uri)-1] == '/')
-      strcat(filename, "home.html");
-    return 1;
-  }
-  else {  /* Dynamic content */
-    ptr = index(uri, '?');
-    if (ptr) {
-      strcpy(cgiargs, ptr+1);
-      *ptr = '\0';
+    if (!strstr(uri, "cgi-bin")) {  /* Static content */
+        strcpy(cgiargs, "");
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        if (uri[strlen(uri)-1] == '/')
+            strcat(filename, "home.html");
+        return 1;
     }
-    else 
-      strcpy(cgiargs, "");
-    strcpy(filename, ".");
-    strcat(filename, uri);
-    return 0;
-  }
+    else {  /* Dynamic content */
+        ptr = index(uri, '?');
+        if (ptr) {
+            strcpy(cgiargs, ptr+1);
+            *ptr = '\0';
+        }
+        else 
+            strcpy(cgiargs, "");
+        strcpy(filename, ".");
+        strcat(filename, uri);
+        return 0;
+    }
 }
 
 /*
@@ -174,69 +174,69 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
  */
 void get_filetype(char *filename, char *filetype) 
 {
-  if (strstr(filename, ".html"))
-    strcpy(filetype, "text/html");
-  else if (strstr(filename, ".gif"))
-    strcpy(filetype, "image/gif");
-  else if (strstr(filename, ".jpg"))
-    strcpy(filetype, "image/jpeg");
-  else
-    strcpy(filetype, "text/plain");
+    if (strstr(filename, ".html"))
+        strcpy(filetype, "text/html");
+    else if (strstr(filename, ".gif"))
+        strcpy(filetype, "image/gif");
+    else if (strstr(filename, ".jpg"))
+        strcpy(filetype, "image/jpeg");
+    else
+        strcpy(filetype, "text/plain");
 }  
 
 /* genrequest - compiles a HTTP request */
 void genrequest(char *request, char *method, char *uri, char *version){
-  /* create request string */
-  strcpy(request,"GET");
-  if (uri[strlen(uri)-1] == '/')
-    strcat(uri, "index.html");
-  strcat(request," ");
-  strcat(request, uri);
-  strcat(request," ");
-  strcat(request, version);
-  strcat(request,"\r\n");
+    /* create request string */
+    strcpy(request,"GET");
+    if (uri[strlen(uri)-1] == '/')
+        strcat(uri, "index.html");
+    strcat(request," ");
+    strcat(request, uri);
+    strcat(request," ");
+    strcat(request, version);
+    strcat(request,"\r\n");
 }
 
 /* genheader - generates a HTTP request header */
 void genheader(char *host,char *header){
-  strcpy(header,"Host: ");
-  strcat(header,host);
-  strcat(header,"\r\n\r\n");
+    strcpy(header,"Host: ");
+    strcat(header,host);
+    strcat(header,"\r\n\r\n");
 }
 
 /*/////////// Added by Ben on 12/2/11*/
 
 /* Given a website, parses the url into its host and argument.
 
-   Input: char* url (Contains the full URL.), char** host (Garbage.), char** uri (Garbage.)
-   Output: char* url (Garbage.), char** host (Host name of the URL. ex: www.cmu.edu.), char** uri (Argument of the URL. Contains the remaining directory of the URL.)
+Input: char* url (Contains the full URL.), char** host (Garbage.), char** uri (Garbage.)
+Output: char* url (Garbage.), char** host (Host name of the URL. ex: www.cmu.edu.), char** uri (Argument of the URL. Contains the remaining directory of the URL.)
 
-   Requires string.h and stdio.h. strchr(s, c) finds the first occurence of char c i
-   n string s.
+Requires string.h and stdio.h. strchr(s, c) finds the first occurence of char c i
+n string s.
 */
 void parseURL(char* url, char* host, char* uri)
 {
-  int len = 0;
-  int pos;
-  int offset = 0; /* http:// has a length of 7. We should really be looking for the first space, but this is still guaranteed to work because it will be the first instance of http */
-  char nohttp[MAXLINE];
-  //  char method[MAXLINE];
+    int len = 0;
+    int pos;
+    int offset = 0; /* http:// has a length of 7. We should really be looking for the first space, but this is still guaranteed to work because it will be the first instance of http */
+    char nohttp[MAXLINE];
+    //  char method[MAXLINE];
 
-  //sscanf(url, "%s %s %s", method, url, version);
-  offset = strcspn(url, "http://") + 7;
-  len = strlen(url);
-  //printf("input -- url: %s\tmethod: %s\tversion: %s\n", url, method, version);
+    //sscanf(url, "%s %s %s", method, url, version);
+    offset = strcspn(url, "http://") + 7;
+    len = strlen(url);
+    //printf("input -- url: %s\tmethod: %s\tversion: %s\n", url, method, version);
 
-  /* Removes the http:// from an url. */
-  strcpy(nohttp, url + offset);
+    /* Removes the http:// from an url. */
+    strcpy(nohttp, url + offset);
 
-  //printf("string nohttp: %s\n", nohttp);
+    //printf("string nohttp: %s\n", nohttp);
 
-  /* Searches for the url by looking for the first slash. */
-  pos = strcspn(nohttp, "/");
-  strncpy(host, nohttp, pos);
-  strncpy(uri, nohttp + pos, len - pos);
+    /* Searches for the url by looking for the first slash. */
+    pos = strcspn(nohttp, "/");
+    strncpy(host, nohttp, pos);
+    strncpy(uri, nohttp + pos, len - pos);
 
-  printf("output: url: %s\thost: %s\t uri: %s\n", url, host, uri);
+    printf("output: url: %s\thost: %s\t uri: %s\n", url, host, uri);
 }
 
